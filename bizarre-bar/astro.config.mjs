0d5ed1a4e9a79c3069904f41cd84ba528055aaee
unknown <seanpnn00@gmail.com>
2023-10-09 19:11:20 +0200

    add svelte + tailwind working

--- bizarre-bar/astro.config.mjs
+++ bizarre-bar/astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
+  renderers: ["@astrojs/renderer-svelte"],
+  output: "server",
+  adapter: vercel(),
 });
---
+++ bizarre-bar/src/components/Button.svelte
@@ -0,0 +1,13 @@
+<script>
+  let message = '';
+
+  export let color ;
+
+</script>
+
+<button on:click={() => message += 'hello'}
+style="background: {color}; padding: 1rem; ">
+Say Hello !
+</button>
+
+{message}
